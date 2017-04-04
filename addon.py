f3682946987a28fa9c4190fd7af931426607f3a7
MathematicalMuscle <jeff13987@gmail.com>
2017-04-04 01:00:49 -0700

    v0.0.9 (2017-04-04) - Automatically modify Exodus using Kodi Callbacks (if installed)

--- addon.py
+++ addon.py
@@ -158,7 +158,7 @@ def done(title, dest, downloaded):
         xbmcgui.Window(10000).clearProperty('GEN-DOWNLOADED')
 
 
-def modify_addons():
+def modify_addons(msg_fmt='ok'):
     # modify Exodus
     infile = xbmc.translatePath('special://home/addons/plugin.video.exodus/exodus.py')
     if xbmcvfs.exists(infile):
@@ -172,18 +172,28 @@ def modify_addons():
             text = text.replace(old, new)
             with open(infile, 'w') as f:
                 f.write(text)
-            xbmcgui.Dialog().ok('Remote Downloader', 'Exodus successfully modified!')
+            if msg_fmt == 'ok':
+                xbmcgui.Dialog().ok('Remote Downloader', 'Exodus successfully modified!')
+            elif msg_fmt == 'notification':
+                xbmc.executebuiltin("XBMC.Notification({0},{1},{2},{3})".format('Remote Downloader', 'Exodus successfully modified!', 5000,
+                                                                                xbmcaddon.Addon('script.remote_downloader').getAddonInfo('icon')))
         elif new in text:
-            xbmcgui.Dialog().ok('Remote Downloader', 'Exodus was already modified.')
+            if msg_fmt == 'ok':
+                xbmcgui.Dialog().ok('Remote Downloader', 'Exodus was already modified.')
         else:
             xbmcgui.Dialog().ok('Remote Downloader', 'Exodus could not be modified.')
 
-    # set Exodus settings
-    xbmcaddon.Addon('plugin.video.exodus').setSetting('downloads', 'true')
-    if xbmcaddon.Addon('plugin.video.exodus').getSetting('movie.download.path') == "":
-        xbmcaddon.Addon('plugin.video.exodus').setSetting('movie.download.path', xbmc.translatePath('special://userdata'))
-    if xbmcaddon.Addon('plugin.video.exodus').getSetting('tv.download.path') == "":
-        xbmcaddon.Addon('plugin.video.exodus').setSetting('tv.download.path', xbmc.translatePath('special://userdata'))
+        # make a file to indicate that Exodus has been modified
+        modfile = xbmcvfs.File(xbmc.translatePath('special://home/addons/plugin.video.exodus/.modified'), 'w')
+        result = modfile.write('Modified by Remote Downloader')
+        modfile.close()
+
+        # set Exodus settings
+        xbmcaddon.Addon('plugin.video.exodus').setSetting('downloads', 'true')
+        if xbmcaddon.Addon('plugin.video.exodus').getSetting('movie.download.path') == "":
+            xbmcaddon.Addon('plugin.video.exodus').setSetting('movie.download.path', xbmc.translatePath('special://userdata'))
+        if xbmcaddon.Addon('plugin.video.exodus').getSetting('tv.download.path') == "":
+            xbmcaddon.Addon('plugin.video.exodus').setSetting('tv.download.path', xbmc.translatePath('special://userdata'))
 
     # modify Last Played
     infile = xbmc.translatePath('special://home/addons/plugin.video.last_played/addon.py')
@@ -205,11 +215,69 @@ def modify_addons():
             text = text.replace(old, new)
             with open(infile, 'w') as f:
                 f.write(text)
-            xbmcgui.Dialog().ok('Remote Downloader', 'Last Played successfully modified!')
+            if msg_fmt == 'ok':
+                xbmcgui.Dialog().ok('Remote Downloader', 'Last Played successfully modified!')
+            elif msg_fmt == 'notification':
+                xbmc.executebuiltin("XBMC.Notification({0},{1},{2},{3})".format('Remote Downloader', 'Last Played successfully modified!', 5000,
+                                                                                xbmcaddon.Addon('script.remote_downloader').getAddonInfo('icon')))
         elif old in text and new in text:
-            xbmcgui.Dialog().ok('Remote Downloader', 'Last Played was already modified.')
+            if msg_fmt == 'ok':
+                xbmcgui.Dialog().ok('Remote Downloader', 'Last Played was already modified.')
         else:
             xbmcgui.Dialog().ok('Remote Downloader', 'Last Played could not be modified.')
+
+    # set Kodi Callbacks settings
+    if xbmcvfs.exists(xbmc.translatePath('special://home/addons/script.service.kodi.callbacks/')) and msg_fmt == 'ok':
+        if xbmcgui.Dialog().yesno('Remote Downloader', "Create 'Kodi Callbacks' Task and Event?"):
+            # task number
+            task = 1
+            task_type = 'builtin'
+            task_builtin = "RunScript(\"script.remote_downloader\", {\"modify_addons_silent\": True})".decode('utf-8')
+            while task < 11:
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('T{0}.type'.format(task)) == 'none' \
+                        or (xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('T{0}.type'.format(task)) == task_type \
+                            and xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('T{0}.builtin'.format(task)) == task_builtin):
+                    break
+                else:
+                    task += 1
+
+            # event number
+            event = 1
+            exodus_folder = xbmc.translatePath('special://home/addons/plugin.video.exodus')
+            while event < 11:
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getLocalizedString(int(xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.type'.format(event)))) == 'None' \
+                        or (xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.type'.format(event)) == '32235' \
+                            and xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.type-v'.format(event)) == '6' \
+                            and xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.folder'.format(event)) == exodus_folder \
+                            and xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.task'.format(event)) == 'Task {0}'.format(task) \
+                            and xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.patterns'.format(event)) == '*.modified'):
+                    break
+                else:
+                    event += 1
+
+            if task < 11 and event < 11:
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('T{0}.builtin'.format(task)) != task_builtin:
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('T{0}.builtin'.format(task), task_builtin)
+
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('T{0}.type'.format(task)) != task_type:
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('T{0}.type'.format(task), task_type)
+
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.type'.format(event)) != '32235':
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('E{0}.type'.format(event), '32235')
+
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.type-v'.format(event)) != '6':
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('E{0}.type-v'.format(event), '6')
+
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.folder'.format(event)) != exodus_folder:
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('E{0}.folder'.format(event), exodus_folder)
+
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.task'.format(event)) != 'Task {0}'.format(task):
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('E{0}.task'.format(event), 'Task {0}'.format(task))
+
+                if xbmcaddon.Addon('script.service.kodi.callbacks').getSetting('E{0}.patterns'.format(event)) != '*.modified':
+                    xbmcaddon.Addon('script.service.kodi.callbacks').setSetting('E{0}.patterns'.format(event), '*.modified')
+
+                xbmcgui.Dialog().ok('Remote Downloader', "Created 'Kodi Callbacks' Task {0} and Event {1}".format(task, event))
 
 
 def getJsonRemote(host, port, username, password, method, parameters):
@@ -257,13 +325,17 @@ def getJsonRemote(host, port, username, password, method, parameters):
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
+        # modify addons and exit
         modify_addons()
-
-        # nothing to download, so exit
         sys.exit()
 
     params = eval(urllib.unquote_plus(sys.argv[1]).replace('streaminfo=', ''))
     xbmc.log('script.remote_downloader: ' + str(params))
+
+    if params.get('modify_addons_silent') is not None:
+        # modify addons silently and exit
+        modify_addons(msg_fmt='notification')
+        sys.exit()
 
     # provided parameters
     image = params.get('image')
